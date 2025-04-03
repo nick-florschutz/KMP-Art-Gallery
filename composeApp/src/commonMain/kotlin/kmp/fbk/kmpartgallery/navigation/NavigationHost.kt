2c0d469c72f6b9142da2b1fe6528f33dbb05347c
Nick Florschutz <njflor99@gmail.com>
2025-04-02 22:30:01 -0400

    removing test screen

--- composeApp/src/commonMain/kotlin/kmp/fbk/kmpartgallery/navigation/NavigationHost.kt
+++ composeApp/src/commonMain/kotlin/kmp/fbk/kmpartgallery/navigation/NavigationHost.kt
@@ -5,7 +5,6 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
-import kmp.fbk.kmpartgallery.TestScreen
 import kmp.fbk.kmpartgallery.features.listscreen.ListScreen
 
 @Composable
