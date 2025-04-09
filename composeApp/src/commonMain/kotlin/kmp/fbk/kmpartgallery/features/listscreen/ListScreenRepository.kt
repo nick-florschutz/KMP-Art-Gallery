ebc5f8bb13593649bf3c08c332907b310ed4980f
Nick Florschutz <njflor99@gmail.com>
2025-04-08 21:39:49 -0400

    uncommenting the call method in ListScreenRepository

--- composeApp/src/commonMain/kotlin/kmp/fbk/kmpartgallery/features/listscreen/ListScreenRepository.kt
+++ composeApp/src/commonMain/kotlin/kmp/fbk/kmpartgallery/features/listscreen/ListScreenRepository.kt
@@ -1,5 +1,8 @@
 package kmp.fbk.kmpartgallery.features.listscreen
 
+import de.jensklingenberg.ktorfit.Callback
+import io.github.aakira.napier.Napier
+import io.ktor.client.statement.HttpResponse
 import kmp.fbk.kmpartgallery.local_storage.dao.DepartmentDao
 import kmp.fbk.kmpartgallery.local_storage.domain_models.Department
 import kmp.fbk.kmpartgallery.local_storage.mappers.toDepartmentEntity
@@ -7,6 +10,7 @@ import kmp.fbk.kmpartgallery.local_storage.mappers.toDepartmentsList
 import kmp.fbk.kmpartgallery.networking.MetArtApi
 import kmp.fbk.kmpartgallery.networking.NetworkRequestBuilder
 import kmp.fbk.kmpartgallery.networking.createMetArtApi
+import kmp.fbk.kmpartgallery.networking.response_data_models.ArtObjectCountAndIds
 import org.koin.mp.KoinPlatform
 
 class ListScreenRepository(
@@ -27,24 +31,23 @@ class ListScreenRepository(
     }
 
 
-//    suspend fun fetchAllObjectIdsCall() {
-//        metArtApi.getAllArtPiecesIdsCall().onExecute(
-//            callBack = object: Callback<ArtObjectCountAndIds> {
-//                override fun onError(exception: Throwable) {
-//                    exception.printStackTrace()
-//                    throw exception
-//                }
-//
-//                override fun onResponse(call: ArtObjectCountAndIds, response: HttpResponse) {
-//                    Napier.i(tag = "RequestBuilder.getAllObjectIdsCall()", message = "response: $response")
-//                    Napier.i(tag = "RequestBuilder.getAllObjectIdsCall()") {
-//                        val data = json.decodeFromString<ArtObjectCountAndIds>(call)
-//                        "data: $data"
-//                    }
-//                }
-//            },
-//        )
-//    }
+    suspend fun fetchAllObjectIdsCall() {
+        metArtApi.getAllArtPiecesIdsCall().onExecute(
+            callBack = object: Callback<ArtObjectCountAndIds> {
+                override fun onError(exception: Throwable) {
+                    exception.printStackTrace()
+                    throw exception
+                }
+
+                override fun onResponse(call: ArtObjectCountAndIds, response: HttpResponse) {
+                    Napier.i(tag = "RequestBuilder.getAllObjectIdsCall()", message = "response: $response")
+                    Napier.i(tag = "RequestBuilder.getAllObjectIdsCall()") {
+                        "data: $call"
+                    }
+                }
+            },
+        )
+    }
 
 
 }
