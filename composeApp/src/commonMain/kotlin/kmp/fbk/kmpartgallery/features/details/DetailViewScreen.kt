15987e31b08405c194cd503ea57e8cdb00c9fe65
Nick Florschutz <njflor99@gmail.com>
2025-04-18 20:34:34 -0400

    working some more on the detail view page

--- composeApp/src/commonMain/kotlin/kmp/fbk/kmpartgallery/features/details/DetailViewScreen.kt
+++ composeApp/src/commonMain/kotlin/kmp/fbk/kmpartgallery/features/details/DetailViewScreen.kt
@@ -4,13 +4,19 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Circle
 import androidx.compose.material.icons.filled.Error
 import androidx.compose.material.icons.filled.FileDownload
 import androidx.compose.material3.CircularProgressIndicator
@@ -20,12 +26,16 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
@@ -36,6 +46,7 @@ import coil3.request.ImageRequest
 import kmp.fbk.kmpartgallery.ViewModelState
 import kmp.fbk.kmpartgallery.domain_models.ArtPiece
 import kmp.fbk.kmpartgallery.extraSmallPadding
+import kmp.fbk.kmpartgallery.getScreenHeight
 import kmp.fbk.kmpartgallery.mediumPadding
 import kmp.fbk.kmpartgallery.smallPadding
 import org.koin.mp.KoinPlatform
@@ -95,62 +106,85 @@ private fun DetailViewScreenContent(
     platformContext: PlatformContext,
     navController: NavController,
 ) {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-    ) {
-        AsyncImage(
-            model = ImageRequest.Builder(platformContext)
-                .data(viewModel.determineWhichImageToUse())
-                .build(),
-            contentDescription = null,
-            placeholder = rememberVectorPainter(Icons.Default.FileDownload),
-            fallback = rememberVectorPainter(Icons.Default.Error),
-            error = rememberVectorPainter(Icons.Default.Error),
-            contentScale = ContentScale.Fit,
-            modifier = Modifier.fillMaxSize()
-        )
+    val screenHeight = getScreenHeight()
+    val imageHeight = remember {
+        screenHeight / 1.7f
+    }
 
-        IconButton(
-            onClick = { navController.popBackStack() },
-            modifier = Modifier
-                .align(Alignment.TopStart)
-                .systemBarsPadding()
-                .padding(start = smallPadding)
-                .background(
-                    color = MaterialTheme.colorScheme.surface.copy(alpha = 0.6f),
-                    shape = RoundedCornerShape(4.dp)
-                )
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+    ) {
+        Box(
+            modifier = Modifier.height(imageHeight)
+                .background(Color.Black),
         ) {
-            Icon(
-                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+            AsyncImage(
+                model = ImageRequest.Builder(platformContext)
+                    .data(viewModel.determineWhichImageToUse())
+                    .build(),
                 contentDescription = null,
-                tint = MaterialTheme.colorScheme.onSurface,
+                placeholder = rememberVectorPainter(Icons.Default.FileDownload),
+                fallback = rememberVectorPainter(Icons.Default.Error),
+                error = rememberVectorPainter(Icons.Default.Error),
+                contentScale = ContentScale.Fit,
+                modifier = Modifier.fillMaxSize()
             )
+
+            IconButton(
+                onClick = { navController.popBackStack() },
+                modifier = Modifier
+                    .align(Alignment.TopStart)
+                    .systemBarsPadding()
+                    .padding(start = smallPadding)
+                    .background(
+                        color = MaterialTheme.colorScheme.surface.copy(alpha = 0.75f),
+                        shape = RoundedCornerShape(4.dp)
+                    )
+            ) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.onSurface,
+                )
+            }
         }
 
         Column(
-            verticalArrangement = Arrangement.spacedBy(extraSmallPadding),
-            modifier = Modifier
-                .align(Alignment.BottomStart)
-                .systemBarsPadding()
-                .padding(horizontal = smallPadding, vertical = mediumPadding)
+            modifier = Modifier.padding(mediumPadding)
         ) {
             Text(
                 text = artPiece.title ?: "No Art Piece Found",
-                color = Color.White,
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold,
                 modifier = Modifier
-                    .background(Color.Black.copy(alpha = 0.6f), RoundedCornerShape(4.dp))
-                    .padding(vertical = extraSmallPadding, horizontal = smallPadding)
             )
-            if (artPiece.artistDisplayName?.isNotBlank() == true) {
-                Text(
-                    text = artPiece.artistDisplayName,
-                    color = Color.White,
-                    modifier = Modifier
-                        .background(Color.Black.copy(alpha = 0.6f), RoundedCornerShape(4.dp))
-                        .padding(vertical = extraSmallPadding, horizontal = smallPadding)
-                )
-            }
+
+           Row(
+               horizontalArrangement = Arrangement.spacedBy(extraSmallPadding),
+               verticalAlignment = Alignment.CenterVertically,
+           ) {
+               Text(
+                   text = artPiece.artistDisplayName?.takeIf { it.isNotBlank() }  ?: "Unknown",
+                   fontSize = 18.sp,
+                   color = if (artPiece.artistDisplayName.isNullOrBlank()) Color.Gray else Color.Blue,
+                   modifier = Modifier
+               )
+
+               Icon(
+                   imageVector = Icons.Default.Circle,
+                   tint = Color.Gray,
+                   contentDescription = null,
+                   modifier = Modifier.size(8.dp)
+               )
+
+               Text(
+                   text = artPiece.objectDate?.takeIf { it.isNotBlank() } ?: "Unknown",
+                   fontSize = 18.sp,
+                   modifier = Modifier
+               )
+           }
         }
     }
 }
