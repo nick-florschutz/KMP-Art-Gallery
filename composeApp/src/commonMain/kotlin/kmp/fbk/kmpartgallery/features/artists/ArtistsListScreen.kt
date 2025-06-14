ed84a6f9bb6c593078e37b83d9ab46328d1e817b
Nick Florschutz <njflor99@gmail.com>
2025-06-14 12:16:40 -0400

    great improvements to the ArtistsListScreen

--- composeApp/src/commonMain/kotlin/kmp/fbk/kmpartgallery/features/artists/ArtistsListScreen.kt
+++ composeApp/src/commonMain/kotlin/kmp/fbk/kmpartgallery/features/artists/ArtistsListScreen.kt
@@ -2,36 +2,48 @@ package kmp.fbk.kmpartgallery.features.artists
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import io.github.aakira.napier.Napier
 import kmp.fbk.kmpartgallery.ViewModelState
+import kmp.fbk.kmpartgallery.extraSmallPadding
+import kmp.fbk.kmpartgallery.getScreenHeight
 import kmp.fbk.kmpartgallery.reusable_ui_compomenents.BasicErrorView
 import kmp.fbk.kmpartgallery.reusable_ui_compomenents.BasicLoadingView
 import kmp.fbk.kmpartgallery.smallPadding
 import kmpartgallery.composeapp.generated.resources.Res
 import kmpartgallery.composeapp.generated.resources.error_loading_artists
 import kmpartgallery.composeapp.generated.resources.no_artists_found
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.stringResource
 import org.koin.mp.KoinPlatform
 
@@ -68,6 +80,7 @@ fun ArtistsScreen(
 private fun ArtistsList(
     artists: Map<String, List<String>>,
     lazyListState: LazyListState = rememberLazyListState(),
+    coroutineScope: CoroutineScope = rememberCoroutineScope(),
 ) {
     if (artists.isEmpty()) {
         Box(
@@ -78,39 +91,77 @@ private fun ArtistsList(
         }
     } else {
 
-        LazyColumn(
-            state = lazyListState,
+        Row(
             modifier = Modifier.fillMaxSize()
         ) {
-            artists.keys.forEach { sectionHeader ->
-                stickyHeader {
-                    Row(
-                        modifier = Modifier
-                            .background(MaterialTheme.colorScheme.surfaceVariant)
-                            .fillMaxWidth()
-                            .wrapContentHeight()
-                            .padding(smallPadding)
-                    ) {
-                        Text(text = sectionHeader)
+            LazyColumn(
+                state = lazyListState,
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+            ) {
+                artists.keys.forEach { sectionHeader ->
+                    stickyHeader {
+                        Row(
+                            modifier = Modifier
+                                .background(MaterialTheme.colorScheme.surfaceVariant)
+                                .fillMaxWidth()
+                                .wrapContentHeight()
+                                .padding(smallPadding)
+                        ) {
+                            Text(text = sectionHeader)
+                        }
+                    }
+                    items(
+                        items = artists[sectionHeader].orEmpty(),
+                        key = { it },
+                    ) { artist ->
+                        ArtistListItem(
+                            artist = artist,
+                            lastArtist = artists[sectionHeader]!!.last() == artist,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .animateItem()
+                        )
                     }
                 }
-                items(
-                    items = artists[sectionHeader].orEmpty(),
-                    key = { it },
-                ) { artist ->
-                    ArtistListItem(
-                        artist = artist,
-                        lastArtist = artists[sectionHeader]!!.last() == artist,
+            }
+
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .wrapContentWidth()
+                    .padding(horizontal = smallPadding)
+                    .verticalScroll(rememberScrollState())
+            ) {
+                artists.keys.forEach { section ->
+                    Text(
+                        text = section,
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .animateItem()
+                            .clickable {
+                                coroutineScope.launch {
+                                    val indicesBeforeTappedSection = artists.keys.indexOf(section)
+                                    var count = 0
+                                    // Calculate number of items before tapped section
+                                    artists.keys.forEach {
+                                        if (artists.keys.indexOf(it) < indicesBeforeTappedSection) {
+                                            count += artists[it]!!.size
+                                        } else {
+                                            return@forEach
+                                        }
+                                    }
+
+                                    lazyListState.scrollToItem(
+                                        index = count,
+                                        scrollOffset = 0,
+                                    )
+                                }
+                            }
+                            .padding(extraSmallPadding)
                     )
                 }
             }
         }
-
-
-
     }
 }
 
